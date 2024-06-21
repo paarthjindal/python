0160487c576c4b1af7ae3262dff195acec92af69
paarthjindal <paarth.jindal@students.iiit.ac.in>
2024-06-21 14:45:46 +0530

    do

--- oop.py
+++ oop.py
@@ -2,3 +2,12 @@
 # all its content and features can be accessed via
 # docs.python.org/3/library/turtle.html
 
+class car:
+    def __init__(self, name, color, mileage):
+        self.name = name
+        self.color = color
+        self.mileage = mileage
+        print(self.name, self.color, self.mileage)
+
+alto = car("mercide", "blue", 100)
+ 
